cc7895765a4cc84738c50e537e597e757f8b2f85
lossp <richard.xxmxx@gmail.com>
2020-04-19 21:37:05 +0800

    修改serverhandler的逻辑，修改接口为SimpleChannelInboundHandler, 方便定义接收消息体

--- server/src/main/java/lossp/handler/ServerHandler.java
+++ server/src/main/java/lossp/handler/ServerHandler.java
@@ -1,10 +1,25 @@
 package lossp.handler;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
+import okhttp3.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ChannelHandler.Sharable
-public class ServerHandler extends ChannelInboundHandlerAdapter {
+// TODO 需要将ByteBuf指定为更加明确的消息VO，目前仅仅是填充逻辑代码，此部分需要优化
+// TODO 此部分需要引入protobuf... 或者定义统一的消息接口。
+public class ServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    Logger logger = LoggerFactory.getLogger(ServerHandler.class);
+
+    private final MediaType mediaType = MediaType.parse("application/json");
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext context, ByteBuf byteBuf) {
+        logger.info("Receiving message = [{}]", byteBuf);
+        // TODO 填充存入
+    }
 
     @Override
     public void channelRead(ChannelHandlerContext context, Object message) {
