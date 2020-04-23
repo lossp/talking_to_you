216528ce354096f45857419ae4d6ac34c26912ff
lossp <richard.xxmxx@gmail.com>
2020-04-23 11:09:10 +0800

    增加sessionHolder类的打印HashMap函数，方便调试

--- server/src/main/java/lossp/session/SessionHolder.java
+++ server/src/main/java/lossp/session/SessionHolder.java
@@ -3,7 +3,9 @@ package lossp.session;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lossp.valueObject.UserInfo;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SessionHolder {
@@ -32,6 +34,32 @@ public class SessionHolder {
 
     public static void saveChannel(Long userId, NioSocketChannel socketChannel) {
         CHANNEL_MAP.put(userId, socketChannel);
+    }
+
+    public static String printChannelMap() {
+        Set<Map.Entry<Long, NioSocketChannel>> entrySet = CHANNEL_MAP.entrySet();
+        Iterator<Map.Entry<Long, NioSocketChannel>> iterator = entrySet.iterator();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (iterator.hasNext()) {
+            Map.Entry<Long, NioSocketChannel> entry = iterator.next();
+            long userId = entry.getKey().longValue();
+            NioSocketChannel channel = entry.getValue();
+            stringBuilder.append("id = " + userId + " , channel = " + channel);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String printSessionMap() {
+        Set<Map.Entry<Long, String>> entrySet = SESSION_MAP.entrySet();
+        Iterator<Map.Entry<Long, String>> iterator = entrySet.iterator();
+        StringBuilder stringBuilder = new StringBuilder();
+        while (iterator.hasNext()) {
+            Map.Entry<Long, String> entry = iterator.next();
+            long userId = entry.getKey().longValue();
+            String username = entry.getValue();
+            stringBuilder.append("id = " + userId + " , username = " + username);
+        }
+        return stringBuilder.toString();
     }
 
     public static void removeChannle(NioSocketChannel socketChannel) {
